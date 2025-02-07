2e4d8bf7d22d9bf2083860ff2325f4e6e72657de
Allan Rasmussen <allanbrondum@gmail.com>
2025-02-07 15:06:23 +0100

    fix

--- src/tree_lut.rs
+++ src/tree_lut.rs
@@ -9,60 +9,183 @@ use tfhe::core_crypto::algorithms::{
 use tfhe::core_crypto::entities::{
     Cleartext, GlweCiphertext, GlweCiphertextMutView, GlweCiphertextOwned, LweCiphertextView,
 };
-use tfhe::core_crypto::prelude::{
-    CiphertextModulus, ComputationBuffers, Fft, GlweSize, LweCiphertextOwned, MonomialDegree,
-    Plaintext, PolynomialSize,
-};
+use tfhe::core_crypto::prelude::*;
 use tfhe::shortint;
 use tfhe::shortint::ciphertext::NoiseLevel;
 use tfhe::shortint::engine::ShortintEngine;
 use tfhe::shortint::server_key::ShortintBootstrappingKey;
-use tfhe::shortint::Ciphertext;
+use tfhe::shortint::{
+    CarryModulus, Ciphertext, ClassicPBSParameters, MaxNoiseLevel, MessageModulus,
+};
+
+const PARAMS: ClassicPBSParameters = ClassicPBSParameters {
+    lwe_dimension: LweDimension(692),
+    glwe_dimension: GlweDimension(4),
+    polynomial_size: PolynomialSize(512),
+    lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+        3.5539902359442825e-06,
+    )),
+    glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+        2.845267479601915e-15,
+    )),
+    pbs_base_log: DecompositionBaseLog(12),
+    pbs_level: DecompositionLevelCount(3),
+    ks_base_log: DecompositionBaseLog(3),
+    ks_level: DecompositionLevelCount(4),
+    message_modulus: MessageModulus(2),
+    carry_modulus: CarryModulus(1),
+    max_noise_level: MaxNoiseLevel::new(20),
+    log2_p_fail: -64.074,
+    ciphertext_modulus: CiphertextModulus::new_native(),
+    encryption_key_choice: EncryptionKeyChoice::Small,
+};
 
 #[derive(Clone)]
-struct FheContext {
+pub struct Shortint1BitFheContext {
     client_key: Arc<shortint::client_key::ClientKey>,
     server_key: Arc<shortint::server_key::ServerKey>,
+    packing_keyswitch_key: Arc<LwePackingKeyswitchKeyOwned<u64>>,
 }
 
-impl FheContext {
-    pub fn test_vector(&self, f: impl Fn(u64) -> u64) -> GlweCiphertextOwned<u64> {
-        test_vector(
+pub struct TestVector(GlweCiphertextOwned<u64>);
+
+impl Shortint1BitFheContext {
+    pub fn generate_keys() -> (Arc<shortint::ClientKey>, Self) {
+        Self::generate_keys_with_params(PARAMS)
+    }
+
+    fn generate_keys_with_params(params: ClassicPBSParameters) -> (Arc<shortint::ClientKey>, Self) {
+        let (client_key, server_key) = shortint::gen_keys(params);
+
+        let packing_keyswitch_key = ShortintEngine::with_thread_local_mut(|engine| {
+            lwe_packing_keyswitch_key_generation::allocate_and_generate_new_lwe_packing_keyswitch_key(
+                &client_key.lwe_secret_key,
+                &client_key.glwe_secret_key,
+                params.ks_base_log,
+                params.ks_level,
+                params.lwe_noise_distribution,
+                params.ciphertext_modulus,
+                &mut engine.encryption_generator,
+            )
+        });
+
+        let context = Shortint1BitFheContext {
+            client_key: client_key.clone().into(),
+            server_key: server_key.into(),
+            packing_keyswitch_key: packing_keyswitch_key.into(),
+        };
+
+        (context.client_key.clone(), context)
+    }
+
+    pub fn test_vector_from_cleartext_fn(&self, f: impl Fn(u64) -> u64) -> TestVector {
+        TestVector(test_vector_from_cleartext_fn(
             self.server_key.bootstrapping_key.polynomial_size(),
             self.server_key.bootstrapping_key.glwe_size(),
             f,
-        )
+        ))
     }
 
-    pub fn bootstrap(&self, ct: &Ciphertext, test_vector: &GlweCiphertextOwned<u64>) -> Ciphertext {
+    pub fn test_vector_from_ciphertexts(
+        &self,
+        ct1: &shortint::Ciphertext,
+        ct2: &shortint::Ciphertext,
+    ) -> TestVector {
+        todo!()
+        // test_vector_from_ciphertexts(
+        //     self.server_key.bootstrapping_key.polynomial_size(),
+        //     self.server_key.bootstrapping_key.glwe_size(),
+        //     ct1, ct2,
+        // )
+    }
+
+    pub fn bootstrap(
+        &self,
+        ct: &shortint::Ciphertext,
+        test_vector: &TestVector,
+    ) -> shortint::Ciphertext {
         let mut ct = ct.clone();
+        self.bootstrap_assign(&mut ct, test_vector);
+        ct
+    }
+
+    pub fn bootstrap_assign(&self, ct: &mut shortint::Ciphertext, test_vector: &TestVector) {
         ShortintEngine::with_thread_local_mut(|engine| {
             let (mut ciphertext_buffers, buffers) = engine.get_buffers(&self.server_key);
 
-            lwe_keyswitch::keyswitch_lwe_ciphertext(
-                &self.server_key.key_switching_key,
-                &ct.ct,
-                &mut ciphertext_buffers.buffer_lwe_after_ks,
-            );
-
             apply_programmable_bootstrap(
                 &self.server_key.bootstrapping_key,
-                &ciphertext_buffers.buffer_lwe_after_ks.as_view(),
-                &mut ct.ct,
-                &test_vector,
+                &ct.ct.as_view(),
+                &mut ciphertext_buffers.buffer_lwe_after_pbs.as_mut_view(),
+                &test_vector.0,
                 buffers,
+            );
+
+            lwe_keyswitch::keyswitch_lwe_ciphertext(
+                &self.server_key.key_switching_key,
+                &ciphertext_buffers.buffer_lwe_after_pbs.as_view(),
+                &mut ct.ct,
             );
         });
 
         ct.set_noise_level(NoiseLevel::NOMINAL, self.server_key.max_noise_level);
-        ct
     }
+
+    pub fn packing_key_switch(&self, cts: &[&shortint::Ciphertext]) -> GlweCiphertextOwned<u64> {
+        let out = ShortintEngine::with_thread_local_mut(|engine| {
+            let in_data: Vec<u64> = cts
+                .iter()
+                .flat_map(|ct| ct.ct.as_ref().iter().copied())
+                .collect();
+
+            let ins = LweCiphertextListOwned::from_container(
+                in_data,
+                self.packing_keyswitch_key
+                    .input_key_lwe_dimension()
+                    .to_lwe_size(),
+                CiphertextModulus::new_native(),
+            );
+            let mut out = GlweCiphertextOwned::new(
+                0,
+                self.packing_keyswitch_key.output_glwe_size(),
+                self.packing_keyswitch_key.output_polynomial_size(),
+                CiphertextModulus::new_native(),
+            );
+            lwe_packing_keyswitch::keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphertext(
+                &self.packing_keyswitch_key,
+                &ins,
+                &mut out,
+            );
+            out
+        });
+
+        out
+    }
+
+    // pub fn packing_key_switch_single(&self, ct: &shortint::Ciphertext) -> GlweCiphertextOwned<u64> {
+    //     let out = ShortintEngine::with_thread_local_mut(|engine| {
+    //         let mut out = GlweCiphertextOwned::new(
+    //             0,
+    //             self.packing_keyswitch_key.output_glwe_size(),
+    //             self.packing_keyswitch_key.output_polynomial_size(),
+    //             CiphertextModulus::new_native(),
+    //         );
+    //         lwe_packing_keyswitch::keyswitch_lwe_ciphertext_into_glwe_ciphertext(
+    //             &self.packing_keyswitch_key,
+    //             &ct.ct,
+    //             &mut out,
+    //         );
+    //         out
+    //     });
+    //
+    //     out
+    // }
 }
 
 fn apply_programmable_bootstrap(
     bootstrapping_key: &ShortintBootstrappingKey,
     in_buffer: &LweCiphertextView<u64>,
-    out_buffer: &mut LweCiphertextOwned<u64>,
+    out_buffer: &mut LweCiphertextMutView<u64>,
     acc: &GlweCiphertext<Vec<u64>>,
     buffers: &mut ComputationBuffers,
 ) {
@@ -85,7 +208,7 @@ fn apply_programmable_bootstrap(
 fn apply_blind_rotate(
     bootstrapping_key: &ShortintBootstrappingKey,
     in_buffer: &LweCiphertextView<u64>,
-    acc: &mut GlweCiphertext<&mut [u64]>,
+    acc: &mut GlweCiphertextMutView<u64>,
     buffers: &mut ComputationBuffers,
 ) {
     let ShortintBootstrappingKey::Classic(fourier_bsk) = bootstrapping_key else {
@@ -119,7 +242,16 @@ fn encode(clear: Cleartext<u64>) -> Plaintext<u64> {
     Plaintext(clear.0 << 62)
 }
 
-fn test_vector(
+// fn decode(plain: Plaintext<u64>) -> Cleartext<u64> {
+//     Cleartext((plain.0 >> 62) & 1)
+// }
+
+fn decode(plain: Plaintext<u64>) -> Cleartext<u64> {
+    let decomposer = SignedDecomposer::new(DecompositionBaseLog(2), DecompositionLevelCount(1));
+    Cleartext(decomposer.closest_representable(plain.0) >> 62)
+}
+
+fn test_vector_from_cleartext_fn(
     polynomial_size: PolynomialSize,
     glwe_size: GlweSize,
     f: impl Fn(u64) -> u64,
@@ -151,15 +283,61 @@ fn test_vector(
     let half_box_size = box_size / 2;
 
     // Negate the first half_box_size coefficients
-    for a_i in body_slice[0..half_box_size].iter_mut() {
-        *a_i = (*a_i).wrapping_neg();
-    }
+    // for a_i in body_slice[0..half_box_size].iter_mut() {
+    //     *a_i = (*a_i).wrapping_neg();
+    // }
+
+    // for a_i in body_slice.iter_mut() {
+    //     *a_i = (*a_i).wrapping_neg() + encode(Cleartext(1)).0;
+    // }
 
     // Rotate the accumulator
     body_slice.rotate_left(half_box_size);
 
     acc
 }
+
+// fn test_vector_from_ciphertexts(
+//     polynomial_size: PolynomialSize,
+//     glwe_size: GlweSize,
+//     ct1: &shortint::Ciphertext, ct2: &shortint::Ciphertext
+// ) -> GlweCiphertextOwned<u64> {
+//     let mut acc = GlweCiphertext::new(
+//         0,
+//         glwe_size,
+//         polynomial_size,
+//         CiphertextModulus::new_native(),
+//     );
+//
+//     // Modulus of the msg contained in the msg bits and operations buffer
+//     let input_modulus_sup = 2;
+//
+//     // N/(p/2) = size of each block
+//     let box_size = polynomial_size.0 / input_modulus_sup;
+//
+//     let mut acc_body = acc.get_mut_body();
+//     let mut body_slice = acc_body.as_mut();
+//
+//     // Tracking the max value of the function to define the degree later
+//
+//     for i in 0..input_modulus_sup {
+//         let index = i * box_size;
+//         let f_eval = f(i as u64);
+//         body_slice[index..index + box_size].fill(encode(Cleartext(f_eval)).0);
+//     }
+//
+//     let half_box_size = box_size / 2;
+//
+//     // Negate the first half_box_size coefficients
+//     for a_i in body_slice[0..half_box_size].iter_mut() {
+//         *a_i = (*a_i).wrapping_neg();
+//     }
+//
+//     // Rotate the accumulator
+//     body_slice.rotate_left(half_box_size);
+//
+//     acc
+// }
 
 #[cfg(test)]
 mod test {
@@ -168,43 +346,37 @@ mod test {
     use tfhe::core_crypto::prelude::*;
     use tfhe::shortint::{CarryModulus, ClassicPBSParameters, MaxNoiseLevel, MessageModulus};
 
-    // 1 bit
-    const PARAMS1: ClassicPBSParameters = ClassicPBSParameters {
-        lwe_dimension: LweDimension(692),
-        glwe_dimension: GlweDimension(4),
-        polynomial_size: PolynomialSize(512),
-        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
-            3.5539902359442825e-06,
-        )),
-        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
-            2.845267479601915e-15,
-        )),
-        pbs_base_log: DecompositionBaseLog(12),
-        pbs_level: DecompositionLevelCount(3),
-        ks_base_log: DecompositionBaseLog(3),
-        ks_level: DecompositionLevelCount(4),
-        message_modulus: MessageModulus(2),
-        carry_modulus: CarryModulus(1),
-        max_noise_level: MaxNoiseLevel::new(20),
-        log2_p_fail: -64.074,
-        ciphertext_modulus: CiphertextModulus::new_native(),
-        encryption_key_choice: EncryptionKeyChoice::Big,
-    };
+    #[test]
+    fn test_packing_key_switch() {
+        let (client_key, context) = Shortint1BitFheContext::generate_keys();
 
-    fn keys() -> (Arc<shortint::ClientKey>, FheContext) {
-        let (client_key, server_key) = shortint::gen_keys(PARAMS1);
+        let ct0_clear = 0;
+        let ct1_clear = 1;
+        let ct0 = client_key.encrypt(ct0_clear);
+        let ct1 = client_key.encrypt(ct1_clear);
 
-        let context = FheContext {
-            client_key: client_key.clone().into(),
-            server_key: server_key.into(),
-        };
+        let packed = context.packing_key_switch(&[&ct0, &ct1]);
+        let mut packed_plain = PlaintextList::new(0, PlaintextCount(packed.polynomial_size().0));
+        glwe_encryption::decrypt_glwe_ciphertext(
+            &client_key.glwe_secret_key,
+            &packed,
+            &mut packed_plain,
+        );
 
-        (context.client_key.clone(), context)
+        assert_eq!(decode(Plaintext(packed_plain.as_ref()[0])), Cleartext(0), "plain: {:064b}", packed_plain.as_ref()[0]);
+        assert_eq!(decode(Plaintext(packed_plain.as_ref()[1])), Cleartext(1), "plain: {:064b}", packed_plain.as_ref()[1]);
+        assert_eq!(decode(Plaintext(packed_plain.as_ref()[2])), Cleartext(0), "plain: {:064b}", packed_plain.as_ref()[1]);
+        assert_eq!(decode(Plaintext(packed_plain.as_ref()[3])), Cleartext(0), "plain: {:064b}", packed_plain.as_ref()[1]);
+        assert_eq!(decode(Plaintext(packed_plain.as_ref()[4])), Cleartext(0), "plain: {:064b}", packed_plain.as_ref()[1]);
+
     }
+
+
+
 
     #[test]
     fn test_tree_lut_2() {
-        let (client_key, context) = keys();
+        let (client_key, context) = Shortint1BitFheContext::generate_keys();
 
         let f = |idx0: u64, idx1: u64| -> u64 {
             const TABLE: [[u64; 2]; 2] = [[1, 0], [1, 0]];
@@ -216,14 +388,8 @@ mod test {
         let m0 = client_key.encrypt(m0_clear);
         let m1 = client_key.encrypt(m1_clear);
 
-        let tv_0 = context.test_vector(|idx0| f(idx0, 0));
-        let tv_1 = context.test_vector(|idx0| f(idx0, 1));
-
-        // let lut_0 = context.server_key.generate_lookup_table(|idx0| f(idx0, 0));
-        // let lut_1 = context.server_key.generate_lookup_table(|idx0| f(idx0, 1));
-        //
-        // let d_0 = context.server_key.apply_lookup_table(&m0, &lut_0);
-        // let d_1 = context.server_key.apply_lookup_table(&m0, &lut_1);
+        let tv_0 = context.test_vector_from_cleartext_fn(|idx0| f(idx0, 0));
+        let tv_1 = context.test_vector_from_cleartext_fn(|idx0| f(idx0, 1));
 
         let d_0 = context.bootstrap(&m0, &tv_0);
         let d_1 = context.bootstrap(&m0, &tv_1);
